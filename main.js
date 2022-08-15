70265a56560904b1f79a0af37873b7063f9af755
JuanJo <juanjosemolina19@gmail.com>
2022-08-14 21:21:26 -0500

    se crean la lista de productos desde js de manera dinamica

--- main.js
+++ main.js
@@ -4,36 +4,94 @@ const mobileMenu = document.querySelector(".mobile-menu");
 const iconMobile = document.querySelector(".menu");
 const menuCartIcon = document.querySelector(".navbar-shopping-cart")
 const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
 
 navEmail.addEventListener("click", toggleDesktopMenu);
 iconMobile.addEventListener("click", toggleMobileMenu);
 menuCartIcon.addEventListener("click", toggleShoppingCar);
 
-function toggleDesktopMenu(){
+function toggleDesktopMenu() {
     const isAsideClosed = aside.classList.contains("inactive");
 
-    if(!isAsideClosed){
+    if (!isAsideClosed) {
         aside.classList.add("inactive");
     }
     desktopMenu.classList.toggle("inactive");
 }
 
-function toggleMobileMenu(){
+function toggleMobileMenu() {
     const isAsideClosed = aside.classList.contains("inactive");
 
-    if(!isAsideClosed){
+    if (!isAsideClosed) {
         aside.classList.add("inactive");
     }
     mobileMenu.classList.toggle("inactive");
 
 }
 
-function toggleShoppingCar(){
+function toggleShoppingCar() {
     const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
-    
-    if(!isMobileMenuClosed){
+
+    if (!isMobileMenuClosed) {
         mobileMenu.classList.toggle('inactive');
     }
 
     aside.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Bike Electric',
+    price: 600,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'MTB',
+    price: 300,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+function renderProductsList(arr) {
+    for (product of productList) {
+        const productCard = document.createElement('div');
+        productCard.className = 'product-card'
+
+        const img = document.createElement('img');
+        img.setAttribute('src', product.img);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$ ' + product.price;
+
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+        productInfoFigure.appendChild(productImgCart);
+
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+
+        productCard.appendChild(img);
+        productCard.appendChild(productInfo);
+
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProductsList(productList);   
